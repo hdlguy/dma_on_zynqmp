b1e6b54cf46d96baa0155d25fe1a4d0dff169e8d
my name <test@email.com>
2025-01-01 10:12:39 +0000

    adding datagen control register

--- apps/include/fpga.h
+++ apps/include/fpga.h
@@ -4,12 +4,6 @@
 
 #define     FPGA_REG_OFFSET         0x00000000
 
-// register numbers
-#define     FPGA_ID                 0
-#define     FPGA_VERSION            1
-
-
-
 #define     TEST_RAM_OFFSET         0x00010000
 #define     TEST_RAM_SIZE           0x00001000 // 4k
 
@@ -19,4 +13,9 @@
 #define     DATA_RAM2               0x00040000
 #define     DATA_RAM3               0x00050000
 
+
+// register numbers
+#define     FPGA_ID                 0
+#define     FPGA_VERSION            1
+#define     DGEN_CONTROL            2   // [0]=dg_enable, [4]=dg_ready(read-only), [8]=dg_clear
 
